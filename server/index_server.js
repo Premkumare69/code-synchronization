d7c3f3f38bcb0729717a1a0dfdad07c8bb687784
Premkumare69 <premkumare.ece2020@citchennai.net>
2023-03-14 13:22:32 +0530

    commit 01

--- server/index_server.js
+++ server/index_server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(
     cors({
-        origin:["http://localhost:5000", "http://code-synchronization.onrender.com"],
+        origin: ["http://localhost:5000", "http://code-synchronization.onrender.com"],
     })
 );
 
